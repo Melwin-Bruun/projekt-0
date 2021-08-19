72127880297e939925528af183fb349da8f457e3
melwin-bruun-nti-johanneberg <melwin.bruun@elev.ga.ntig.se>
2021-08-19 16:01:48 +0200

    rätt

--- functions.rb
+++ functions.rb
@@ -1,25 +1,31 @@
 require "sinatra"
 require "sinatra/flash"
-require_relative "seed.rb"
+require 'sqlite3'
+#require_relative "seed.rb"
 #Fetches the class from database
 #and saves them in an array
 enable :sessions
+
+def connect_to_db()
+    db = SQLite3::Database.new('DB/database.db')
+    db.results_as_hash = true 
+    return db 
+end 
+
 get '/' do 
     erb :start
 end
 
 post '/' do
-    @guess_count
     grade_search = params[:class_name] 
     db = connect_to_db()
-    grade = db.execute("SELECT * FROM student WHERE grade = ?", [grade_search])
+    students = db.execute("SELECT * FROM student WHERE grade = ?", [grade_search])
 
-    if grade.empty?
+    if !students.any?
         flash[:error] = "This class does not exist. Try another class!"
         redirect back
     else 
-        @grade = grade
-        @guess_count = -1
+        @student = students.first
         erb :guess
     end
 end
@@ -33,7 +39,7 @@ get '/start' do
     erb :start
 end 
 
-get '/guess' do
+get '/guess/:id' do
     erb :guess
 end
 get '/finish' do
