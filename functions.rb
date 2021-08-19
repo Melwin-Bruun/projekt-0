e8f096fa041d8a0a3e3a7a2b1208bb516613e832
evelinajansen <89136827+evelinajansen@users.noreply.github.com>
2021-08-19 12:59:00 +0200

    erb

--- functions.rb
+++ functions.rb
@@ -2,20 +2,16 @@ require "sinatra"
 require_relative "DB/seed.rb"
 #Fetches the class from database
 #and saves them in an array
-post('/start') do
-    grade_search = params[:class_name] 
-    grade = db.execute("SELECT * FROM students WHERE grade = ?", [grade_search])
-    if grade_search == grade 
-        redirect '/guess'
-    else 
-        #skriv ut felmeddelande
-    end
-    
-
-
-#checks class
-post('/start') do
-    arrayname[grade].each do |grade|
-        if 
+get '/' do 
+    erb :start
 end
 
+# post('/start') do
+#     grade_search = params[:class_name] 
+#     grade = db.execute("SELECT * FROM students WHERE grade = ?", [grade_search])
+#     if grade_search == grade 
+#         redirect '/guess'
+#     else 
+#        hej
+#     end
+# end
