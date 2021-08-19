03a717de8be6f309c408ba70689ed6769eb48834
evelinajansen <89136827+evelinajansen@users.noreply.github.com>
2021-08-19 11:31:16 +0200

    idfk

--- functions.rb
+++ functions.rb
@@ -2,9 +2,12 @@ require "sinatra"
 
 #Fetches the class from database
 #and saves them in an array
-
-#checks class
-post '/start' do
-    arrayname[grade].each do |grade|
-        if 
-end
+post('/start') do
+    grade_search = params[:class_name] 
+    grade = db.execute("SELECT * FROM students WHERE grade = ?", [grade_search])
+    if grade_search == grade 
+        redirect '/guess'
+    else 
+        #skriv ut felmeddelande
+    end
+    
