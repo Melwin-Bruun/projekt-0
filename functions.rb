a1b13d969331590ad3ee8a4a03cf74cf169fbd01
evelinajansen <89136827+evelinajansen@users.noreply.github.com>
2021-08-19 14:05:38 +0200

    idek

--- functions.rb
+++ functions.rb
@@ -5,19 +5,19 @@ require_relative "seed.rb"
 #and saves them in an array
 enable :sessions
 get '/' do 
-    p flash
     erb :start
 end
 
 post '/' do
     grade_search = params[:class_name] 
     db = connect_to_db()
-    grade = db.execute("SELECT * FROM student WHERE grade = ?", [grade_search])
+    @grade = db.execute("SELECT * FROM student WHERE grade = ?", [grade_search])
 
-    if grade.empty?
+    if @grade.empty?
         flash[:error] = "This class does not exist. Try another class!"
         redirect back
     else 
         erb :guess
     end
 end
+
