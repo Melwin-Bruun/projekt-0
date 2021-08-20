a99a390ad6893d8d21da8a41b4789388119026a2
evelinajansen <89136827+evelinajansen@users.noreply.github.com>
2021-08-20 12:50:13 +0200

    func

--- functions.rb
+++ functions.rb
@@ -7,28 +7,91 @@ require 'sqlite3'
 enable :sessions
 
 def connect_to_db()
-    db = SQLite3::Database.new('DB/database.db')
-    db.results_as_hash = true 
-    return db 
-end 
-
-get '/' do 
+        db = SQLite3::Database.new('DB/database.db')
+        db.results_as_hash = true 
+        return db 
+    end 
+get '/' do
     erb :start
 end
-
 post '/' do
-    grade_search = params[:class_name] 
+    class_guess = params[:class_name]
+    student_ids_string = []
     db = connect_to_db()
-    students = db.execute("SELECT * FROM student WHERE grade = ?", [grade_search])
+    students = db.execute("SELECT id FROM student WHERE grade = ?", [class_guess])
 
     if !students.any?
         flash[:error] = "This class does not exist. Try another class!"
         redirect back
     else 
-        @student = students.first
+        students.each do |student|
+            student_ids_string << student['id'] 
+        end
+        @current_student_id = student_ids_string.sample
+        @student_ids_string = student_ids_string.join(",")
+        @student = db.execute("SELECT * FROM student WHERE id = ?", [@current_student_id])
         erb :guess
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def connect_to_db()
+#     db = SQLite3::Database.new('DB/database.db')
+#     db.results_as_hash = true 
+#     return db 
+# end 
+
+# get '/' do 
+#     erb :start
+# end
+
+# post '/' do
+#     grade_search = params[:class_name] 
+#     db = connect_to_db()
+#     students = db.execute("SELECT * FROM student WHERE grade = ?", [grade_search])
+
+#     if !students.any?
+#         flash[:error] = "This class does not exist. Try another class!"
+#         redirect back
+#     else 
+#         @student = students.first
+#         erb :guess
+#     end
+# end
 
 get '/wrong' do 
     erb :wrong
@@ -43,14 +106,14 @@ get '/guess' do
     erb :guess
 end
 
-post '/quess/:id' do
-    #hämta student från db
-    #jämför med vad som finns i params
+# post '/quess/:id' do
+#     #hämta student från db
+#     #jämför med vad som finns i params
 
-    #om rätt
-    #hämta ny student från db
+#     #om rätt
+#     #hämta ny student från db
 
-end
+# end
 
 get '/finish' do
     erb :finish
